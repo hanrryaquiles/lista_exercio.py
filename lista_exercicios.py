03f3711590e704e57c64b854e7059cb38a4ca759
hanrryaquiles <hanrryaquiless@gmail.com>
2025-10-21 00:15:06 -0300

    Add files via upload
    Lista de exercícios

---
+++ lista_exercicios.py
@@ -0,0 +1,16 @@
+numeros = [10, 20, 30, 40, 50]
+print("Lista inicial:", numeros)
+
+print("Primeiro número:", numeros[0])
+print("Número do meio:", numeros[2])
+print("Último número:", numeros[-1])
+
+numeros[1] = 25
+print("Lista atualizada:", numeros)
+
+novo_num = int(input("Digite um número para adicionar à lista: "))
+numeros.append(novo_num)
+print("Lista atualizada:", numeros)
+
+numeros.remove(30)
+print("Lista após remover o 30:", numeros)
